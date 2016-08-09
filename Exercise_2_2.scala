067edc157810881d965dac6f152676fef06a9a36
wsf <wsf.fletcher@gmail.com>
2016-08-09 19:44:46 -0400

    Add file for next exercise
    Add comments to describe excerise requirements.

---
+++ Exercise_2_2.scala
@@ -0,0 +1,4 @@
+// Implement isSorted, which checks whether an Array[A] is sorted according
+//    to a given comparision function
+// def isSorted[A](as: Array[A], ordered: (A,A) => Boolean): Boolean
+//
