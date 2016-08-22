7625d1abe789b76feeb1699a51e714fdbc5cbc2b
wsf <wsf.fletcher@gmail.com>
2016-08-22 19:53:55 -0400

    Method for Collatz Conjecture
    This method tests an integer N for reduction to 1 by dividing N by 2 if
    N is even, or calculating N*3 +1 if N is odd (repeat).

---
+++ src/test/scala/test_collatzConjecture.scala
@@ -0,0 +1,16 @@
+import org.scalatest.FlatSpec
+
+//import FunProgBook.src.main.scala.CollatzConjecture._
+
+class CollatzSpec extends FlatSpec{
+
+
+"A result" should "have a value of true" in {
+  val result = true
+  assert(result === true)
+}
+
+
+
+
+}
